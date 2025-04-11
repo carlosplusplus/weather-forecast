be304e33e65f8f369a5e9980e5fb61a14955d54d
Carlos Lazo <carlos.jose.lazo@gmail.com>
2025-04-11 17:57:35 -0500

    🚧 [WIP] Add Turbo Stream support for weather forecast updates with mock data

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,21 +1,53 @@
 class ForecastsController < ApplicationController
   def index
-    return unless params[:address].present?
+  end
 
-    begin
-      location = GeocodingService.location_data_from_address(params[:address])
-      forecast = WeatherService.new(
-        lat: location[:lat],
-        lon: location[:lon],
-        zip: location[:zip]
-      ).fetch_forecast
+  # def search
+  #   return unless params[:address].present?
 
-      @address = params[:address]
-      @forecast = forecast
-    rescue GeocodingAddressError, GeocodingZipCodeError => e
-      flash[:alert] = e.message
-    rescue => e
-      flash[:alert] = "An error occurred while fetching the forecast: #{e.message}."
+  #   begin
+  #     location = GeocodingService.location_data_from_address(params[:address])
+  #     forecast = WeatherService.new(
+  #       lat: location[:lat],
+  #       lon: location[:lon],
+  #       zip: location[:zip]
+  #     ).fetch_forecast
+
+  #     @address = params[:address]
+  #     @forecast = forecast
+
+  #     respond_to do |format|
+  #       format.turbo_stream do
+  #         render turbo_stream: turbo_stream.replace("forecast_results", partial: "forecasts/weather_report", locals: { forecast: @forecast })
+  #       end
+  #     end
+  #   rescue GeocodingAddressError, GeocodingZipCodeError => e
+  #     flash[:alert] = e.message
+  #   rescue => e
+  #     flash[:alert] = "An error occurred while fetching the forecast: #{e.message}."
+  #   end
+  # end
+
+  def search
+    Rails.logger.debug "Request format: #{request.format}"
+    address = params[:address]
+
+    # Replace this with actual logic to fetch weather data
+    @forecast = {
+      current: { temperature: 72, description: "Sunny" },
+      today: { high: 75, low: 65 },
+      extended: [
+        { name: "Monday", temperature: 70, temperature_unit: "F", short_forecast: "Partly Cloudy" },
+        { name: "Tuesday", temperature: 68, temperature_unit: "F", short_forecast: "Rainy" }
+      ],
+      from_cache: false
+    }
+    @address = address
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("forecast_results", partial: "forecasts/weather_report", locals: { forecast: @forecast, address: @address })
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "forecasts#index"
+  get "forecasts/search", to: "forecasts#search", as: "search_forecasts"
 end
