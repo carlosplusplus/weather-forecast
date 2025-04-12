9618573b06c264acafca7c6db31e75808a7247b8
Carlos Lazo <carlos.jose.lazo@gmail.com>
2025-04-11 21:38:35 -0500

    🚧 [WIP] Add weather icons and helper to display the current weather

--- app/helpers/forecasts_helper.rb
+++ app/helpers/forecasts_helper.rb
@@ -1,2 +1,27 @@
 module ForecastsHelper
+
+# This method generates an HTML image tag for a weather icon based on the short forecast description.
+# It uses a case statement to match various weather conditions and returns the corresponding icon file name.
+  def weather_icon(short_forecast, size = 14)
+    icon_file = case short_forecast.downcase
+                when /rain/i # Matches "Rain", "Light Rain", "Heavy Rain", etc.
+                  "rain.png"
+                when /snow/i # Matches "Snow", "Light Snow", "Heavy Snow", etc.
+                  "snow.png"
+                when "partly sunny"
+                  "partly_sunny.png"
+                when "partly cloudy"
+                  "partly_cloudy.png"
+                when /thunderstorm/ # Matches "Thunderstorm", "Severe Thunderstorm", etc.
+                  "thunderstorm.png"
+                when /cloudy/
+                  "cloudy.png"
+                when /sunny/ # Matches "Sunny", "Mostly Sunny", etc.
+                  "sunny.png"
+                else
+                  "sunny.png" # Fallback for unknown conditions
+                end
+
+    image_tag("weather_icons/#{icon_file}", alt: short_forecast, class: "w-#{size} h-#{size} mx-auto")
+  end
 end
