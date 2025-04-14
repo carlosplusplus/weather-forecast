db15aaae0bf3d176592076a6c900515cec7422f8
Carlos Lazo <carlos.jose.lazo@gmail.com>
2025-04-13 23:48:54 -0500

    Remove test/ folder given the use of rspec

--- test/controllers/forecasts_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ForecastsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get forecasts_index_url
-    assert_response :success
-  end
-end
